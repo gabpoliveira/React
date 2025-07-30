4b684ab32874e14808633a65a68830618ad62002
Gabriel Oliveira <gaoliveira154@gmail.com>
2025-07-29 21:39:28 -0300

    Seção 4 - CSS e React

--- 4_CSS_e_React/css_react/src/App.js
+++ 4_CSS_e_React/css_react/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './components/MyComponent';
+import Title from './components/Title';
 
 function App() {
+  const n = 15;
+  const redTitle = false;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* CSS Global */}
+      <h1>CSS no React</h1>
+      {/* CSS de Componente */}
+      <MyComponent />
+      {/* Inline CSS */}
+      <p style={{ color: "white", backgroundColor: "black"}}>CSS INLINE</p>
+      {/* CSS inline dinâmico */}
+      <h2 style={n < 10 ? {color:"purple" , backgroundColor: "#3a404b"} : {color: "pink", backgroundColor: "#3a404b"}}>CSS inline dinâmico</h2>
+      <h2 style={n > 10 ? {color:"purple", backgroundColor: "#3a404b"} : {color: "pink", backgroundColor: "#3a404b"}}>CSS inline dinâmico</h2>
+      {/* Classe dinâmica */}
+      <h2 className={redTitle ? "red-title" : "title"}>Título com classe dinâmica</h2>
+      {/* CSS Modules */}
+      <Title />
     </div>
   );
 }
---
+++ 4_CSS_e_React/css_react/src/components/MyComponent.js
@@ -0,0 +1,11 @@
+import "./MyComponent.css"
+
+const MyComponent = () => {
+  return (
+    <div>
+        <h1 className="my-comp-h1">Css de componente</h1>
+    </div>
+  )
+}
+
+export default MyComponent
